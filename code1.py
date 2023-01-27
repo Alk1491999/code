59cb64c654f871189630cd8a782222f95e81ba17
Alk1491999 <72344476+Alk1491999@users.noreply.github.com>
2023-01-27 17:20:32 +0530

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,5 @@
+def pairsum(list1, target):
+    for i in range (len(list1)-1):
+        for j in range(i+1, len(list1)):
+            if list1[i]+list1[j]==target:
+                return(list1[i], list1[j])
